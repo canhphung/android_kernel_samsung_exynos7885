068ebbcc42f460b96f4e13536c8d496f2b9449cb
Canh Phung <gcon5827@gmail.com>
2024-07-22 09:38:30 +0700

    Create build_kernel.sh

---
+++ build_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+
+export ARCH=arm64
+export CROSS_COMPILE=~/toolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export ANDROID_MAJOR_VERSION=q
+
+make exynos7885-a7y18lte_defconfig
+make -j$(nproc --all)
